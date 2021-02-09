f7fd5ae2462eabf9cdf9698bf5468e788e17a9fe
vitor9 <vtorzy@gmail.com>
2021-02-08 21:12:58 -0300

    Criado a classe de transacao e algumas regras de negocio

--- codepix/domain/model/transaction.go
+++ codepix/domain/model/transaction.go
@@ -1,2 +1,67 @@
 package model
 
+import (
+	"errors"
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+const (
+	TransactionPending 		string = "pending"
+	TransactionCompleted 	string = "completed"
+	TransactionError 		string = "error"
+	TransactionConfirmed 	string = "confirmed"
+)
+
+type Transactions struct {
+	Transaction []Transaction
+}
+
+type Transaction struct {
+	Base 				`valid:"required"`
+	AccountFrom 		*Account `valid:"-"`
+	Amount			 	float64 `json:"amount" valid:"notnull"`
+	PixKeyTo 			*PixKey `valid"-"`
+	Status 				string `json:"status" valid:"notnull"`
+	Description 		string `json:"description" valid:"notnull"`
+	CancelDescription 	string `json:"cancel_description" valid:"notnull"`
+}
+
+func (transaction *Transaction) isValid() error {
+	_,err := govalidator.ValidateStruct(transaction)
+
+	if transaction.Amount <= 0 {
+		return errors.New("the amount must be greater than 0")
+	}
+
+	if transaction.Status != TransactionPending && transaction.Status != TransactionCompleted && transaction.Status != TransactionError {
+		return errors.New("invalid status for the transaction")
+	}
+
+	if transaction.PixKeyTo.AccountID == transaction.AccountFrom.ID {
+		return errors.New("the source and destination account cannot be the same")
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewTransaction(accountFrom *Account, amount float64, pixKeyTo *PixKey, description string) (*Transaction, error) {
+	transaction := Transaction{
+		AccountFrom: 	accountFrom,
+		Amount: 		amount,
+		PixKeyTo:   	pixKeyTo,
+		Status:    		TransactionPending,
+		Description: 	description,
+	}
+	transaction.ID = uuid.NewV4().String()
+	transaction.CreatedAt = time.Now()
+	err := transaction.isValid()
+	if err != nil {
+		return nil, err
+	}
+	return &transaction, nil
+}
